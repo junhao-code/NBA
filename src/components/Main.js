0b9cff1007db4d5d85138512092c632bab782f7d
jus074 <jus074@eng.ucsd.edu>
2017-12-19 22:06:22 -0800

    finish stage 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,7 +20,7 @@ export class Main extends React.Component {
   render() {
     return (
         <div className="main">
-          <Profile/>
+          <Profile playerInfo={this.state.playerInfo} playerId={this.state.playerId}/>
           <ShotChart playerId={this.state.playerId}/>
         </div>
     );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { PROFILE_PIC_URL_PREFIX, TEAM_PIC_URL_PREFIX } from "../constants"
 export class Profile extends React.Component {
+
   render() {
+    const {
+      teamAbbreviation,
+      teamCity,
+      teamName,
+      playerName,
+      height,
+      weight,
+      pts, reb, ast, pie,
+    } = this.props.playerInfo;
     return(
       <div className='profile'>
-
+        <img className="profile-pic" src={ `${PROFILE_PIC_URL_PREFIX}/${this.props.playerId}.png`} alt="player"/>
+        <img className="team-logo" src={ `${TEAM_PIC_URL_PREFIX}/${teamAbbreviation}_logo.svg`} alt="team"/>
       </div>
     );
   }
